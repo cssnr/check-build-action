f13ab3e9e2a3dd46a2d0f0de9dbdac4faedad00a
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2025-11-08 09:52:41 -0800

    Updates (#10)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const core = require('@actions/core')
 const exec = require('@actions/exec')
 const github = require('@actions/github')
 
-const { Pull } = require('./api')
+const { Pull } = require('./api.js')
 
 ;(async () => {
     try {
